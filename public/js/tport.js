bba4ba172a14f15a0b8f28e82d556b076c801ac8
Joosung Ahn <joosung21@gmail.com>
2018-03-27 15:59:45 +0900

    Add Theme Color

--- public/js/tport.js
+++ public/js/tport.js
@@ -1,3 +1,55 @@
+// DEMO ONLY: Change Theme Color
+var changeTheme = function(color){
+  removeAllTheme();
+  switch(color) {
+    case 'dark':
+      $.cookie("theme", "dark");
+      break;
+    case 'white':
+      $.cookie("theme", "white");
+      $('.navbar').addClass('theme-white');
+      $('footer').addClass('theme-white');
+      break;
+    case 'red':
+      $.cookie("theme", "red");
+      $('.navbar').addClass('theme-red');
+      $('footer').addClass('theme-red');
+      break;
+    case 'orange':
+      $.cookie("theme", "orange");
+      $('.navbar').addClass('theme-orange');
+      $('footer').addClass('theme-orange');
+      break;
+    case 'yellow':
+      $.cookie("theme", "yellow");
+      $('.navbar').addClass('theme-yellow');
+      $('footer').addClass('theme-yellow');
+      break;
+    case 'green':
+      $.cookie("theme", "green");
+      $('.navbar').addClass('theme-green');
+      $('footer').addClass('theme-green');
+      break;
+    case 'blue':
+      $.cookie("theme", "blue");
+      $('.navbar').addClass('theme-blue');
+      $('footer').addClass('theme-blue');
+      break;
+    case 'navy':
+      $.cookie("theme", "navy");
+      $('.navbar').addClass('theme-navy');
+      $('footer').addClass('theme-navy');
+      break;
+    default:
+      break;
+  }
+}
+
+var removeAllTheme = function(){
+  $('.navbar').removeClass('theme-white theme-dark theme-red theme-orange theme-yellow theme-green theme-blue theme-navy');
+  $('footer').removeClass('theme-white theme-dark theme-red theme-orange theme-yellow theme-green theme-blue theme-navy');
+}
+
 
 
 // Document Ready
@@ -22,8 +74,46 @@ $( document ).ready(function() {
     return false;
   });
 
+  // DEMO ONLY: Find Theme color from Cookie
+  switch($.cookie("theme")) {
+    case 'white':
+      $('.navbar').addClass('theme-white');
+      $('footer').addClass('theme-white');
+      break;
+    case 'dark':
+      $('.navbar').addClass('theme-dark');
+      $('footer').addClass('theme-dark');
+      break;
+    case 'red':
+      $('.navbar').addClass('theme-red');
+      $('footer').addClass('theme-red');
+      break;
+    case 'orange':
+      $('.navbar').addClass('theme-orange');
+      $('footer').addClass('theme-orange');
+      break;
+    case 'yellow':
+      $('.navbar').addClass('theme-yellow');
+      $('footer').addClass('theme-yellow');
+      break;
+    case 'green':
+      $('.navbar').addClass('theme-green');
+      $('footer').addClass('theme-green');
+      break;
+    case 'blue':
+      $('.navbar').addClass('theme-blue');
+      $('footer').addClass('theme-blue');
+      break;
+    case 'navy':
+      $('.navbar').addClass('theme-navy');
+      $('footer').addClass('theme-navy');
+      break;
+    default:
+      break;
+  }
 
 });
+
 
 
 $(document).scroll(function () {
